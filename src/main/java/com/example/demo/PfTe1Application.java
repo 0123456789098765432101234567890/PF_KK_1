a8363227de1059e36b5f8c60aa318f0172502eb4
K <smghank@icloud.com>
2024-05-12 15:26:29 +0900

    save1

---
+++ src/main/java/com/example/demo/PfTe1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfTe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfTe1Application.class, args);
+	}
+
+}
---
+++ src/main/java/form/LoginForm.java
@@ -0,0 +1,12 @@
+package form;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+	
+	private String loginId;
+	
+	private String password;
+	
+}
